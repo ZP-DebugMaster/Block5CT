bd02fbb97085e6f823fc5574b0e21d202ce145f7
ZP-DebugMaster <77686455+ZP-DebugMaster@users.noreply.github.com>
2021-05-07 11:49:43 -0600

    Add files via upload

--- tkinterTest.py
+++ tkinterTest.py
@@ -17,24 +17,49 @@ def exportList():
         for item in playlistList:
             f.write("%s/n" % item)
 
+def clearWindow():
+    for widgets in top.winfo_children():
+        widgets.destroy()
 
+def mainMenu():
+    clearWindow()
+    LMain = Label(top, text = "B5 GUI Projects")
+    LMain.grid(column = 2, row = 1)
 
-#This is a label widget
-L1 = Label(top, text = "Playlist Maker")
-L1.grid(column = 0, row = 1)
+    B1Main = Button(text = "Week 1", bg = "white", command = week1)
+    B1Main.grid(column = 2, row = 2)
+    
+    B2Main = Button(text = "Week 2", bg = "white")
+    B2Main.grid(column = 2, row = 3)
 
-#This is an entry widget
-E1 = Entry(top, bd = 5)
-E1.grid(column = 0, row = 2)
+    B3Main = Button(text = "Week 3", bg = "white")
+    B3Main.grid(column = 2, row = 4)
 
-#This is a button widget
-B1 = Button(text = "  Print Playlist    ", bg = "#c6edee", command = results )
-B1.grid(column = 0, row = 3)
+def week1():
+    clearWindow()
+    #This is a label widget
+    L1 = Label(top, text = "Playlist Maker")
+    L1.grid(column = 0, row = 1)
 
-B2 = Button(text = " + ", bg = "#f7cac9", command = addToList)
-B2.grid(column = 1, row = 2)
+    #This is an entry widget
+    E1 = Entry(top, bd = 5)
+    E1.grid(column = 0, row = 2)
 
-B3 = Button(text = "Export List", bg = "#ceb0af", command = export)
-B3.grid(column = 0, row = 4)
+    #This is a button widget
+    B1 = Button(text = "  Print Playlist    ", bg = "#c6edee", command = results )
+    B1.grid(column = 0, row = 3)
 
-top.mainloop()
+    B2 = Button(text = " + ", bg = "#f7cac9", command = addToList)
+    B2.grid(column = 1, row = 2)
+
+    B3 = Button(text = "Export List", bg = "#ceb0af", command = exportList)
+    B3.grid(column = 0, row = 4)
+
+    """
+    Bclear = Button(text = "Clear Window", bg = "white", command = clearWindow)
+    Bclear.grid(column = 3, row = 1)
+    """
+
+if __name__== "__main__":
+    mainMenu()
+    top.mainloop()
